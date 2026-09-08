7a0c7f6a9e427dca30810cdaf426243f2bd42258
Alejandro <alejandrocoxaj94@gmail.com>
2026-09-08 17:09:41 -0600

    Alejandro importó las librerías PySide

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QLabel,
+    QVBoxLayout,
+    QDialog,
+    QFormLayout,
+    QLineEdit,
+    QComboBox,
+    QSpinBox,
+    QPushButton,
+    QColorDialog,
+    QFileDialog,
+    QMessageBox
+)
